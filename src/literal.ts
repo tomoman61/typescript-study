4eddce00af2ab1818275b6f9639c125a6c369cd4
tomoman <inotomo5@gmail.com>
2020-11-29 06:47:51 +0900

    literal

---
+++ src/literal.ts
@@ -0,0 +1,18 @@
+export {};
+
+let dayOfTheWeek: '日'|'月'|'火'|'水'|'木'|'金'|'土'  = '日';
+dayOfTheWeek = '月';
+dayOfTheWeek = '火';
+dayOfTheWeek = '水';
+dayOfTheWeek = '木';
+dayOfTheWeek = '金';
+dayOfTheWeek = '土';
+// dayOfTheWeek = '31';
+
+let month: 1|2|3|4|5|6|7|8|9|10|11|12 = 1;
+month = 12;
+// month = 13;
+// month = -1;
+
+let TRUE: true = true;
+// let TRUE2: true = false;
