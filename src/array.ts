05a5d39af071f05ab03d5807bfbbfdee25aed0ee
tomoman <inotomo5@gmail.com>
2020-11-28 10:21:11 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,14 @@
+export { };
+
+let numbers: number[] = [1, 2, 3];
+
+console.log({numbers});
+
+let nijigenhairetu: number[][] = [
+  [50, 100],
+  [150, 200]
+]
+console.log({nijigenhairetu});
+
+let hairetu: (string | number | boolean)[] = [1, false, "Japan"];
+console.log(hairetu);
